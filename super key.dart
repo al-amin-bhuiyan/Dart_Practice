190a727bc697ab45fcbf53f413f4f261293d7991
al-amin-bhuiyan <alexal018827@gmail.com>
2025-09-29 05:45:17 +0600

    2nd

--- super key.dart
+++ super key.dart
@@ -1,16 +1,17 @@
 class Super_key {
-  Super_key(String name, int age) {
+  Super_key({required String name, required int age}) {
     print((name));
     print("I am from parent class");
   }
 }
 
 class subclass extends Super_key {
-  subclass() : super("Shobuj", 25) {
+  subclass({required String name, required int age})
+    : super(name: name, age: age) {
     print("from subclass");
   }
 }
 
 void main() {
-  subclass sub = subclass();
+  subclass sub = subclass(name: 'Shobuj', age: 25);
 }
