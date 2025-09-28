6768e6a2600511702cc1f2a3fc52aa96a77b2714
al-amin-bhuiyan <alexal018827@gmail.com>
2025-09-29 05:30:31 +0600

    superkey

--- constructor.dart
+++ constructor.dart
@@ -1,15 +1,28 @@
-class Constructor {
-  Constructor() {
-    print("Constructor");
+class school {
+  int x = 4;
+  String? name;
+  int? age;
+  List<String> name_of_the_student = [];
+  void display() {
+    print(x);
+  }
+
+  school() {
+    name_of_the_student.add("Shobuj");
+    name_of_the_student.add("al-amin");
+    name_of_the_student.addAll(["kuddus", "ali "]);
+    name_of_the_student =
+        name_of_the_student.map((e) => e + " bhuiyan").toList();
+    print(name_of_the_student);
   }
 }
 
-class Default_Constructor extends Constructor {
-  Default_Constructor() : super() {
-    print("Default Constructor");
+class student extends school {
+  student() {
+    print(x);
   }
 }
 
 void main() {
-  Default_Constructor c = Default_Constructor();
+  student st = student();
 }
---
+++ super key.dart
@@ -0,0 +1,5 @@
+class Super_key {
+  Super_key(String name, int age) {}
+}
+
+void main() {}
